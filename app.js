3d72ed71c5226eaa1753a9204b0f8241b81fa4bb
maechabin <mitoppo@gmail.com>
2019-07-04 15:27:26 +0900

    wip

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 (function() {
-  // Model
-  var Staff = Backbone.Model.extend({
+  var app = {};
+
+  app.Todo = Backbone.Model.extend({
     defaults: {
-      name: '',
-      age: 0,
-      updateTime: new Date(),
-    },
-    initialize: function() {
-      console.log('Staff[' + this.cid + ']: ' + JSON.stringify(this));
+      title: '',
+      completed: false,
     },
   });
 
-  var tmpStaff = new Staff();
-  tmpStaff.set({ name: 'Murata', age: 15, id: 101 });
+  var todo = new app.Todo({ title: 'Learn Backbone.js', completed: false });
 
-  console.log('Staff[' + tmpStaff.cid + ']: ' + JSON.stringify(tmpStaff));
-
-  tmpStaff.set({ name: 'Murata', age: 115, id: 101 });
-
-  console.log('Staff[' + tmpStaff.cid + ']: ' + JSON.stringify(tmpStaff));
-
-  var tmpStaff2 = new Staff({ name: 'Kenichiro', age: 35, id: 102 });
-
-  // Collection
-  var Staffs = Backbone.Collection.extend({
-    model: Staff,
-  });
-
-  var staffs = new Staffs([tmpStaff, tmpStaff2]);
-
-  console.log('staffs: ' + JSON.stringify(staffs));
-  console.log('staffs.get(cid): ' + JSON.stringify(staffs.get('c1')));
-  console.log('staffs.at(index): ' + JSON.stringify(staffs.at(1)));
-  console.log('staffs.get(id): ' + JSON.stringify(staffs.get(102)));
+  todo.get('title');
+  console.log(todo.get('title'));
+  todo.get('completed');
+  console.log(todo.get('completed'));
+  todo.get('created_at');
+  console.log(todo.get('created_at'));
+  todo.set('created_at', Date());
+  todo.get('created_at');
+  console.log(todo.get('created_at'));
 })();
