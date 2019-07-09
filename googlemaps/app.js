5f504e5c5ba21616cccc961b1a02dde7166f11c3
maeda <takanori@stroly.jp>
2019-07-09 19:37:45 +0900

    google map機能追加

--- googlemaps/app.js
+++ googlemaps/app.js
@@ -1,29 +1,144 @@
 (function() {
+  // Model
+  const Marker = Backbone.Model.extend({
+    defaults: {
+      title: 'marker',
+      latlng: [-34.397, 150.644],
+    },
+  });
+
+  // Collection
+  const Markers = Backbone.Collection.extend({
+    model: Marker,
+    initialize() {
+      console.log('aaa');
+    },
+  });
+
+  // View
   const MapView = Backbone.View.extend({
     el: '#content',
     template: _.template($('#googlemaps-template').html()),
     initialize() {
+      this.collection.on('add', this.handleAdd, this);
       this.render();
     },
     style: {
       width: '50%',
     },
     initMap() {
+      const [lat, lng] = this.model.get('latlng');
       const mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
+        center: new google.maps.LatLng(lat, lng),
         zoom: 8,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
       };
       const mapDiv = this.el.querySelector('.googlemap');
 
       this.map = new google.maps.Map(mapDiv, mapOptions);
+
+      google.maps.event.addListener(this.map, 'click', event => {
+        this.collection.add({
+          title: '',
+          latlng: [event.latLng.lat(), event.latLng.lng()],
+        });
+      });
+    },
+    initMarker(marker) {
+      if (marker) {
+        const [lat, lng] = marker.get('latlng');
+        this.addMarker(lat, lng);
+        return;
+      }
+      const bounds = new google.maps.LatLngBounds();
+      this.collection.each(mark => {
+        const [lat, lng] = mark.get('latlng');
+        const marker = this.addMarker(lat, lng);
+        bounds.extend(marker.getPosition());
+      });
+      this.map.fitBounds(bounds);
+    },
+    addMarker(lat, lng) {
+      const marker = new google.maps.Marker({
+        map: this.map,
+        draggable: false,
+        position: { lat, lng },
+        title: this.model.get('title'),
+      });
+      return marker;
+    },
+    initPolyline() {
+      const directionsService = new google.maps.DirectionsService();
+      const directionsRenderer = new google.maps.DirectionsRenderer();
+      const [origin, ...rest] = this.collection.each(marker => marker);
+      const originLatLng = origin.get('latlng');
+      const destinationLatLng = rest.pop().get('latlng');
+      const waypointsLatLng = rest.map(point => {
+        return {
+          location: new google.maps.LatLng(
+            point.get('latlng')[0],
+            point.get('latlng')[1],
+          ),
+        };
+      });
+
+      /** polylineをレンダリングする際のオプション */
+      directionsRenderer.setOptions({
+        suppressMarkers: true, // マーカーを非表示にする場合はtrue
+        preserveViewport: true, // zoomさせないようにする場合はtrue
+        polylineOptions: {
+          strokeOpacity: 1.0, // polylineの透明度0.0~1.0
+          strokeWeight: 6, // polylineの幅（ピクセル）
+          strokeColor: '#6991fd', // polylineの色
+        },
+      });
+
+      /** directions apiへのリクエスト */
+      directionsService.route(
+        {
+          origin: new google.maps.LatLng(originLatLng[0], originLatLng[1]), // 出発地
+          waypoints: waypointsLatLng, // 経路（配列）
+          destination: new google.maps.LatLng(
+            destinationLatLng[0],
+            destinationLatLng[1],
+          ), // 到着地
+          travelMode: google.maps.TravelMode.DRIVING,
+        },
+        (response, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            // directions apiのレスポンスをセット
+            directionsRenderer.setDirections(response);
+
+            directionsRenderer.setMap(this.map); // polylineを地図に表示
+            return;
+          }
+          return console.log(`status: ${status}`);
+        },
+      );
+    },
+    handleAdd(marker) {
+      this.initMarker(marker);
+      this.initPolyline();
     },
     render() {
       this.$el.html(this.template(this.style));
       this.initMap();
+      this.initMarker();
+      this.initPolyline();
       return this;
     },
   });
 
-  var mapView = new MapView();
+  const marker = new Marker();
+  const markers = new Markers([
+    {
+      title: '',
+      latlng: [-34.397, 150.644],
+    },
+    {
+      title: '',
+      latlng: [-34.197, 150.544],
+    },
+  ]);
+  const mapView = new MapView({ model: marker, collection: markers });
 })();
