d9d8a536bf5481f77e59a98f3215438c37b26478
maeda <takanori@stroly.jp>
2019-07-12 22:38:50 +0900

    Modelのapi試し

--- helloworld/helloworld.js
+++ helloworld/helloworld.js
@@ -1,23 +1,51 @@
 (function() {
+  const Who = Backbone.Model.extend({
+    defaults: {
+      who: 'world',
+    },
+    url: '../',
+    save(key, val, options) {
+      console.log(this);
+      return Backbone.Model.prototype.save.apply(this, arguments);
+    },
+  });
+
   // View
-  var AppView = Backbone.View.extend({
+  const AppView = Backbone.View.extend({
     // el - stands for element. Every view has a element associate in with HTML
     //      content will be rendered.
     el: '#container',
 
+    events: {
+      click: 'handleClick',
+    },
+
     // template which has the placeholder 'who' to be substitute later
-    template: _.template("<h3>Hello <%= who %></h3>"),
+    template: _.template('<h3>Hello <%= who %></h3>'),
 
     // It's the first function called when this view it's instantiated.
-    initialize: function () {
+    initialize: function() {
       this.render();
+      this.model.save(null, {
+        success(model, resp, options) {
+          console.log(arguments);
+        },
+        error(model, resp, options) {
+          console.log(arguments);
+        },
+      });
     },
     // $el - it's a cached jQuery object (el), in which you can use jQuery functions
     //       to push content. Like the Hello World in this case.
-    render: function () {
-      this.$el.html(this.template({ who: 'world!' }));
-    }
+    render: function() {
+      this.$el.html(this.template(this.model.toJSON()));
+    },
+
+    handleClick() {
+      alert('click');
+    },
   });
 
-  var appView = new AppView();
+  const who = new Who();
+  const appView = new AppView({ model: who });
 })();
